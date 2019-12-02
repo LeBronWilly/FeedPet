475531c645ca44bcdbb9c32de758ac09ca5ec1ae
sunny <s1041539@gm.pu.edu.tw>
2019-12-02 19:36:36 +0800

    hotel/model.py新增欄位

--- FeedPet/hotel/models.py
+++ FeedPet/hotel/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Hotel(models.Model):
+    
+    sequence = models.CharField(max_length=15)
+    
+    year_eva = models.CharField(max_length=8)
+    
+    name = models.CharField(max_length=20)
+    
+    postalcode = models.CharField(max_length=5)
+    
+    area = models.CharField(max_length=3)
+    
+    address = models.CharField(max_length=25)
+    
+    def __str__(self):
+     
+        return self.name
+    
