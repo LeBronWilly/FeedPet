7219549226aeb2393d08f8a7be45c1a1793993d9
sunny <s1041539@gm.pu.edu.tw>
2019-12-03 19:50:42 +0800

    改hotel/model.py

--- FeedPet/hotel/models.py
+++ FeedPet/hotel/models.py
@@ -1,21 +1,33 @@
 from django.db import models
-
+from master.models import Master
 # Create your models here.
 class Hotel(models.Model):
+
+    hname = models.CharField(max_length=8)
     
-    sequence = models.CharField(max_length=15)
+    rank = models.CharField(max_length=2)
     
-    year_eva = models.CharField(max_length=8)
-    
-    name = models.CharField(max_length=20)
+    full_name = models.CharField(max_length=12)
+  
+    incharge = models.CharField(max_length=3)
+
+    phone = models.CharField(max_length=11)
     
     postalcode = models.CharField(max_length=5)
     
-    area = models.CharField(max_length=3)
+    district = models.CharField(max_length=3)
     
     address = models.CharField(max_length=25)
     
     def __str__(self):
      
-        return self.name
+        return self.hname
     
+class Favor_hotel(models.Model):
+    
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+    master = models.ForeignKey(Master, on_delete=models.CASCADE)
+    
+    def __str__(self):
+     
+        return self.hotel
--- FeedPet/hotel/views.py
+++ FeedPet/hotel/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
-
+import csv
 # Create your views here.
 
+def hoteldetail(request):
+   with open('/Users/liumeihui/Documents/GitHub/FeedPet/FeedPet/hotel/hotel.csv') as hotelfile:
+    rows = csv.DictReader(hotelfile)  
+    for row in rows:
+        return render(request, 'hotel/hoteldetail.html', locals())
 
 def hotel(request):
     return render(request, 'hotel/hotel.html', locals())
-
-
-def hoteldetail(request):
-    return render(request, 'hotel/hotel_detail.html', locals())
 
 
 def hotel_favorite(request):
