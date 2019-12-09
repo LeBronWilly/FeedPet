834c31d73f7f0d2e9e3aa871134967c40b389f6f
kevinbabana <kevin19961009@gmail.com>
2019-12-09 15:56:16 +0800

    fix recommendpage

--- FeedPet/feed/urls.py
+++ FeedPet/feed/urls.py
@@ -25,8 +25,7 @@ urlpatterns = [
 
     path('add_feed_favor/<int:master_id>/<int:feed_id>/', views.add_feed_favor, name='add_feed_favor'),
 
-    path('feed_recommendation/', views.feed_recommendation,
-         name='feed_recommendation'),
+    path('feed_recommendation/', views.feed_recommendation,name='feed_recommendation'),
 
     path('feeding_record/', views.feeding_record, name='feeding_record'),
 ]
